3649c90054709899346b9dc0c9ffe0ab13e7a68d
Elton Vilanculo <39657623+eltonvilanculo@users.noreply.github.com>
2020-04-14 09:34:26 +0200

    Connection from server and client side status

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+const io = require('socket.io')(http)
 const port = process.env.PORT || 3000
 
 app.get('/', function(req, res){
   res.sendFile('index.html',{ root: './public/view' });
 });
+
+io.on('connection',(socket)=>{
+
+  console.log("User is connected ");
+  socket.on('disconnect' , ()=>{
+    console.log('user is offline')
+  })
+
+})
+
 
 http.listen(port, function(){
   console.log(`listening on *:3000`);
